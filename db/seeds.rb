db13fa69d6f29fd4cad0aaa11581f3f3db88ecd3
AdamManselll <adammansellhmb@gmail.com>
2020-10-28 18:51:14 -0700

    Clean up seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,12 @@
 # frozen_string_literal: true
 
-require 'faker'
-
-class Seed
-  def self.begin
-    seed = Seed.new
-    seed.generate_locations
-    # seed2= Seed.new
-    # seed2.generate_reviews
-  end
-
-  def generate_locations
-    20.times do |i|
-      location = Location.create!(
-        country: Faker::Address.country,
-        city: Faker::Address.city
-      )
-      review = Review.create!(rating: 10, review_text: "I saw #{Faker::TvShows::BreakingBad.character} there", location_id: location.id, user_name: 'Jimbo')
-      puts 'created review_text'
-      puts "Location #{i}: City is #{location.city} in the country of '#{location.country}'."
-    end
-  end
-
-  # def generate_reviews
-  #   20.times do |i|
-  #     review = location.Review.create!(:rating => 10,:review_text => "I saw #{Faker::TvShows::BreakingBad.character} there", :location_id => 1, :user_name => "Jimbo" )
-  #     puts "created #{review_text}"
-  #   end
-  # end
+20.times do |i|
+  location = Location.create!(
+    country: Faker::Address.country,
+    city: Faker::Address.city
+  )
+  5.times do
+    location.reviews.create!(rating: 10, review_text: "I saw #{Faker::TvShows::BreakingBad.character} there", user_name: Faker::Name.name)
+  end      
+  puts "Location #{i}: City is #{location.city} in the country of '#{location.country}'."
 end
-
-Seed.begin
